a11d91c6ae9020ec09bdc0ff965c67443484649b
BeidanDev <franco.peralta7@hotmail.com>
2023-07-06 00:37:53 -0300

    first commit

---
+++ NetCoreLogin/Controllers/ApiController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreLogin.Models;
+
+namespace NetCoreLogin.Controllers
+{
+    public class APIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("api/login")]
+        public ActionResult Index(string email, string password)
+        {
+            if (email == "test@test.com" && password == "123456789")
+            {
+                return Json(new { success = true, message = "ok", redirectUrl = Url.Action("Index", "Home") });
+            }
+            else if (email != "test@test.com")
+            {
+                return Json(new { success = false, message = "invalid email" });
+            }
+            else if(password != "123456789")
+            {
+                return Json(new { success = false, message = "invalid password" });
+            }
+            else
+            {
+                return Json(new { success = false, message = "invalid login" });
+            }
+        }
+    }
+}
---
+++ NetCoreLogin/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace NetCoreLogin.Models
+{
+    public class LoginViewModel
+    {
+        //[Required(ErrorMessage = "El campo {0} es requerido")]
+        //[EmailAddress(ErrorMessage = "El campo debe ser un correo electrónico válido")]
+        public string Email { get; set; }
+
+        //[Required(ErrorMessage = "El campo {0} es requerido")]
+        //[DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ErrorMessage { get; set; } = "";
+    }
+}
